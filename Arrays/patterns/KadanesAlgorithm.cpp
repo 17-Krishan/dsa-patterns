0ef0f76b422a16994ad208a95d69705d4cdce543
Krishan <chaudharykrishan502@gmail.com>
2026-05-28 22:57:05 +0530

    Added kadanes Algo

---
+++ Arrays/patterns/KadanesAlgorithm.cpp
@@ -0,0 +1,18 @@
+// Maximum Subarray Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    int currSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < arr.size(); i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+
+    cout << maxSum;
+}
